9ed9db01874bd0af4c12a0e99ad4767dfd99ad2f
marketmon <etmarkwalter@gmail.com>
2024-07-10 12:56:54 -0400

    pnpm fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ const Home = () => {
     <div className="">
       <h1 className="font-bold text-2xl p-4">Panbo Dummy Application: DayLily</h1>
       <h3 className="pl-4 text-lg">Stay up to date with your co-founders</h3>
+      <p>Day One:</p>
 
       <div>
 
